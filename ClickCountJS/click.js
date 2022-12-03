fc32356d858ee059bfc941e86baf780026dca62d
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2022-12-03 18:08:28 +0100

    Clicker JS fixed (window.onload)

--- ClickCountJS/click.js
+++ ClickCountJS/click.js
@@ -1,6 +1,8 @@
+window.onload = function(){ 
   let button = document.getElementById("clicker"),
   count = 0;
 button.onclick = function() {
   count += 1;
   button.innerHTML = "Click me: " + count;
+};
 };
