a4c1ddfe7cf1bf4737b3c7ace0c2c9987d3a16cf
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2022-12-05 22:57:11 +0100

    Click Counter JS with different values

--- ClickCountJS/click.js
+++ ClickCountJS/click.js
@@ -1,8 +1,18 @@
 window.onload = function(){ 
-  let button = document.getElementById("clicker"),
+  let button = document.getElementById("clicker")
+  let buttontwo = document.getElementById("tenclick")
+
   count = 0;
 button.onclick = function() {
   count += 1;
-  button.innerHTML = "Click me: " + count;
+  button.innerHTML = "+1 = " + count;
+  buttontwo.innerHTML = "+10"
+
 };
+buttontwo.onclick = function() {
+  count += 10;
+  button.innerHTML = "You added ten = " + count;
+  buttontwo.innerHTML = "You added ten = "
+};
+
 };
