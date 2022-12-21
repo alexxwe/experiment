e2791d0aceb98ef47bcbbe5782a3a5c0f6076720
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2022-12-21 17:08:10 +0100

    King and Night movement and winning function

--- Chess/chess.js
+++ Chess/chess.js
@@ -46,3 +46,126 @@ coloring()
 
 
 
+
+            // KING
+
+            if (item.innerText == `${toggle}king`) {
+
+
+                if (aside < 8) {
+                    document.getElementById(`b${a + 1}`).style.backgroundColor = 'green'
+
+                }
+                if (aside > 1) {
+
+                    document.getElementById(`b${a - 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800) {
+
+                    document.getElementById(`b${a + 100}`).style.backgroundColor = 'green'
+                }
+                if (aup > 100) {
+
+                    document.getElementById(`b${a - 100}`).style.backgroundColor = 'green'
+                }
+
+                if (aup > 100 && aside < 8) {
+
+                    document.getElementById(`b${a - 100 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aup > 100 && aside > 1) {
+
+                    document.getElementById(`b${a - 100 - 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800 && aside < 8) {
+
+                    document.getElementById(`b${a + 100 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aup < 800 && aside > 1) {
+
+                    document.getElementById(`b${a + 100 - 1}`).style.backgroundColor = 'green'
+                }
+
+                item.style.backgroundColor = 'pink'
+
+            }
+
+            // KNIGHT
+
+            if (item.innerText == `${toggle}knight`) {
+
+
+                if (aside < 7 && aup < 800) {
+                    document.getElementById(`b${a + 100 + 2}`).style.backgroundColor = 'green'
+                }
+                if (aside < 7 && aup > 200) {
+                    document.getElementById(`b${a - 100 + 2}`).style.backgroundColor = 'green'
+                }
+                if (aside < 8 && aup < 700) {
+                    document.getElementById(`b${a + 200 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aside > 1 && aup < 700) {
+                    document.getElementById(`b${a + 200 - 1}`).style.backgroundColor = 'green'
+                }
+                if (aside > 2 && aup < 800) {
+                    document.getElementById(`b${a - 2 + 100}`).style.backgroundColor = 'green'
+                }
+                if (aside > 2 && aup > 100) {
+                    document.getElementById(`b${a - 2 - 100}`).style.backgroundColor = 'green'
+                }
+                if (aside < 8 && aup > 200) {
+                    document.getElementById(`b${a - 200 + 1}`).style.backgroundColor = 'green'
+                }
+                if (aside > 1 && aup > 200) {
+                    document.getElementById(`b${a - 200 - 1}`).style.backgroundColor = 'green'
+                }
+
+                item.style.backgroundColor = 'pink'
+
+            }
+        
+
+
+        // Toggling the turn
+
+        if (tog % 2 !== 0) {
+            document.getElementById('tog').innerText = "White's Turn"
+            whosTurn('W')
+        }
+        if (tog % 2 == 0) {
+            document.getElementById('tog').innerText = "Black's Turn"
+            whosTurn('B')
+        }
+
+        reddish()
+
+
+
+        // winning()
+
+        numOfKings = 0
+
+
+        document.querySelectorAll('.box').forEach(win => {
+            if (win.innerText == 'Wking' || win.innerText == 'Bking') {
+                numOfKings += 1
+            }
+
+        })
+
+        if (numOfKings == 1) {
+            setTimeout(() => {
+                // console.log(`${toggle}`) 
+                if (tog % 2 == 0) {
+                    alert('White Wins !!')
+                    location.reload()
+                }
+                else if (tog % 2 !== 0) {
+                    alert('Black Wins !!')
+                    location.reload()
+                }
+            }, 100)
+        }
+
+
+
