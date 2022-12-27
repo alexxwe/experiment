c354984bfa32c5eb1491e0d85ee66e7d8ae54da3
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2022-12-27 22:51:05 +0100

    Checkers teams and winning function

--- Checkers/checkers.js
+++ Checkers/checkers.js
@@ -2,7 +2,7 @@ function insertImage(){
 
     document.querySelectorAll('.box').forEach(image =>{
             (image.innerText =='Wdama' || image.innerText == 'Bdama') 
-                image.innerHTML = `${image.innerText} <img class='allimg allpawn' src="${image.innerText}.png" alt="">`
+                image.innerHTML = `${image.innerText} <img class='allimg alldama' src="${image.innerText}.png" alt="">`
                 image.style.cursor = 'pointer'
     })
 }
@@ -35,4 +35,92 @@ coloring()
 
 
 
+//function to not remove the same team element
 
+function reddish() {
+    document.querySelectorAll('.box').forEach(i1 => {
+        if (i1.style.backgroundColor == 'pink') {
+
+            document.querySelectorAll('.box').forEach(i2 => {
+
+                if (i2.style.backgroundColor == 'green' && i2.innerText.length !== 0) {
+
+
+                    greenText = i2.innerText
+
+                    pinkText = i1.innerText
+
+                    pinkColor = ((Array.from(pinkText)).shift()).toString()
+                    greenColor = ((Array.from(greenText)).shift()).toString()
+
+                    getId = i2.id
+                    arr = Array.from(getId)
+                    arr.shift()
+                    aside = eval(arr.pop())
+                    aup = eval(arr.shift())
+                    a = aside + aup
+            
+                    if (a % 2 == 0 && pinkColor == greenColor) {
+                        i2.style.backgroundColor = 'rgb(240, 201, 150)'
+                    }
+                    if (a % 2 !== 0 && pinkColor == greenColor) {
+                        i2.style.backgroundColor = 'rgb(100, 75, 43)'
+                    }
+
+                    // if (pinkColor == greenColor) {
+                    //     i2.style.backgroundColor = 'rgb(253, 60, 60)'
+                    // }
+                }
+            })
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+        // Toggling the turn
+
+        if (tog % 2 !== 0) {
+            document.getElementById('tog').innerText = "White's Turn"
+            whosTurn('W')
+        }
+        if (tog % 2 == 0) {
+            document.getElementById('tog').innerText = "Black's Turn"
+            whosTurn('B')
+        }
+
+        reddish()
+
+
+
+        // winning()
+
+        numOfDama = 0
+
+
+        document.querySelectorAll('.box').forEach(win => {
+            if (win.innerText == 'Wdama' || win.innerText == 'Bdama') {
+                numOfDama += 1
+            }
+
+        })
+
+        if (numOfDama == 1) {
+            setTimeout(() => {
+                // console.log(`${toggle}`) 
+                if (tog % 2 == 0) {
+                    alert('White Wins !!')
+                    location.reload()
+                }
+                else if (tog % 2 !== 0) {
+                    alert('Black Wins !!')
+                    location.reload()
+                }
+            }, 100)
+        }
