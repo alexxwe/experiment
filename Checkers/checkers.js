6a205e76d1129e71abfdd0bddca9ce417225c70d
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2022-12-28 18:01:33 +0100

    checkers pieces implementation

--- Checkers/checkers.js
+++ Checkers/checkers.js
@@ -78,10 +78,110 @@ function reddish() {
 
 
 
+tog = 1
+
+document.querySelectorAll('.box').forEach(item => {
 
 
 
+    item.addEventListener('click', function () {
 
+        // To delete the opposite element
+
+        if (item.style.backgroundColor == 'green' && item.innerText.length == 0) {
+            tog = tog + 1
+        }
+
+        else if (item.style.backgroundColor == 'green' && item.innerText.length !== 0) {
+
+            document.querySelectorAll('.box').forEach(i => {
+                if (i.style.backgroundColor == 'pink') {
+                    pinkId = i.id
+                    pinkText = i.innerText
+
+                    document.getElementById(pinkId).innerText = ''
+                    item.innerText = pinkText
+                    coloring()
+                    insertImage()
+                    tog = tog + 1
+                    
+                }
+            })
+        }
+
+
+
+        getId = item.id             //get id
+        arr = Array.from(getId)     //convert getid to array
+        arr.shift()                 //delete first array element with shiift()
+        aside = eval(arr.pop())     //evaluates the last array element with eval() funcion and assigns it to variable "aside"
+        arr.push('0')               //add element at the end of the array with push()
+        aup = eval(arr.join(''))    //evaluates array content as a number with eval() and assigns it to variable "aup"
+        a = aside + aup             //assign to variable "a" the sum of "aside" and "aup"
+
+
+
+        // Function to display the available paths
+
+        function whosTurn(toggle) {
+
+            
+            // DAMA
+
+            if (item.innerText == `${toggle}dama`) {
+
+
+                for (let i = 1; i < 2; i++) {
+                    if (i < (900 - aup) / 100 && i < 9 - aside && document.getElementById(`b${a + i * 100 + i}`).innerText.length == 0) {
+                        document.getElementById(`b${a + i * 100 + i}`).style.backgroundColor = 'green'
+                    }
+                    else if (i < (900 - aup) / 100 && i < 9 - aside && document.getElementById(`b${a + i * 100 + i}`).innerText.length !== 0) {
+                        document.getElementById(`b${a + i * 100 + i}`).style.backgroundColor = 'green'
+                        break
+                    }
+                }
+
+
+                for (let i = 1; i < 2; i++) {
+                    if (i < aup / 100 && i < 9 - aside && document.getElementById(`b${a - i * 100 + i}`).innerText.length == 0) {
+                        document.getElementById(`b${a - i * 100 + i}`).style.backgroundColor = 'green'
+                    }
+                    else if (i < aup / 100 && i < 9 - aside && document.getElementById(`b${a - i * 100 + i}`).innerText.length !== 0) {
+                        document.getElementById(`b${a - i * 100 + i}`).style.backgroundColor = 'green'
+                        break
+                    }
+                }
+
+
+                for (let i = 1; i < 2; i++) {
+                    if (i < (900 - aup) / 100 && i < aside && document.getElementById(`b${a + i * 100 - i}`).innerText.length == 0) {
+                        document.getElementById(`b${a + i * 100 - i}`).style.backgroundColor = 'green'
+                    }
+                    else if (i < (900 - aup) / 100 && i < aside && document.getElementById(`b${a + i * 100 - i}`).innerText.length !== 0) {
+                        document.getElementById(`b${a + i * 100 - i}`).style.backgroundColor = 'green'
+                        break
+                    }
+
+                }
+
+
+                for (let i = 1; i < 2; i++) {
+                    if (i < aup / 100 && i < aside && document.getElementById(`b${a - i * 100 - i}`).innerText.length == 0) {
+                        document.getElementById(`b${a - i * 100 - i}`).style.backgroundColor = 'green'
+                    }
+                    else if (i < aup / 100 && i < aside && document.getElementById(`b${a - i * 100 - i}`).innerText.length !== 0) {
+                        document.getElementById(`b${a - i * 100 - i}`).style.backgroundColor = 'green'
+                        break
+                    }
+                }
+
+
+
+                item.style.backgroundColor = 'pink'
+
+            }
+
+        }
 
 
         // Toggling the turn
@@ -124,3 +224,59 @@ function reddish() {
                 }
             }, 100)
         }
+
+
+
+    })
+
+})
+
+
+
+
+
+// Moving the element
+document.querySelectorAll('.box').forEach(hathiTest => {
+
+    hathiTest.addEventListener('click', function () {
+
+        if (hathiTest.style.backgroundColor == 'pink') {
+
+            pinkId = hathiTest.id
+            pinkText = hathiTest.innerText
+
+            document.querySelectorAll('.box').forEach(hathiTest2 => {
+
+                hathiTest2.addEventListener('click', function () {
+                    if (hathiTest2.style.backgroundColor == 'green' && hathiTest2.innerText.length == 0) {
+                        document.getElementById(pinkId).innerText = ''
+                        hathiTest2.innerText = pinkText
+                        coloring()
+                        insertImage()
+
+                    }
+
+                })
+            })
+
+        }
+
+    })
+
+})
+
+
+
+
+
+
+// Prvents from selecting multiple elements
+z = 0
+document.querySelectorAll('.box').forEach(ee => {
+    ee.addEventListener('click', function () {
+        z = z + 1
+        if (z % 2 == 0 && ee.style.backgroundColor !== 'green') {
+            coloring()
+        }
+    })
+})
