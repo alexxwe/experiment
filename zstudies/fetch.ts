6d6f977ca1e63c0b6fb23ee5ed3b9c6f01cb1140
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-09-25 23:02:19 +0200

    Update fetch.ts

--- zstudies/fetch.ts
+++ zstudies/fetch.ts
@@ -3,6 +3,7 @@ async function main(){
     const url = 'https://pokeapi.co/api/v2/pokemon/torterra'
     const response = await fetch(url)
     const data = await response.json()
+    
     const name = data.name
     const id = data.id
     const height = data.height
@@ -11,6 +12,9 @@ async function main(){
     const moves = data.moves[Math.floor(Math.random()* (71 - 0 + 1) + 0)].move.name
     const stats = data.stats.map(stat => stat.stat.name)
     const type = data.types.map(types => types.type.name)
+
+    //access a specific element called 'tutor' in this direction
+    const path = data.moves[0].version_group_details[2].move_learn_method.name
 
     console.log({name, id, height, image, shiny, type, moves})
     
@@ -23,6 +27,7 @@ async function main(){
     console.log("-------")
     data.types.map(x => x.type.name)
 
+    //to now which type of element is(object, string...)
     // console.log(typeof data.moves)
 }
 main()
