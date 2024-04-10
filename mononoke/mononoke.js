7979f63745197dd3d3640b48bb9e0c6ec9258e6a
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-04-10 17:51:11 +0200

    Update range mononoke.js

--- mononoke/mononoke.js
+++ mononoke/mononoke.js
@@ -2,9 +2,7 @@ const cards = document.querySelector('.cards')
 const images = document.querySelectorAll('.card_img_one, .card_img_two, .card_img_three')
 const backgrounds = document.querySelectorAll('.card_bg_one, .card_bg_two, .card_bg_three')
 
-const range = 50
-
-const calcValue = (a, b) => ((a / b) * range - range / 2).toFixed(1)
+const calcValue = (a, b) => ((a / b) * 50 - 50 / 2).toFixed(1)
 
 document.addEventListener('mousemove', ({ x, y }) => {
     const yValue = calcValue(y, window.innerHeight)
