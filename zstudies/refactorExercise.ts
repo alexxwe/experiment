f48f43923598b12818f4dec6d6c104c003be0e69
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-05-07 18:00:41 +0200

    Update refactorExercise.ts

--- zstudies/refactorExercise.ts
+++ zstudies/refactorExercise.ts
@@ -40,3 +40,44 @@ fetchData(incomingData => {
     logData()
 })
 
+///////////////////////////////////////////
+//After
+
+type Data = string[]
+
+function fetchData(): Promise<Data> {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const incomingData: Data = ['apple', 'banana', 'cherry']
+
+            resolve(incomingData)
+        }, 1000)
+    })
+}
+
+function processData(data: Data): Data {
+    return data.filter(product => product.length > 5)
+}
+
+function logData(data: Data): any {
+    console.log('Data:', data)
+}
+
+function capitalizeData(data: Data): Data {
+    return data.map(product => product.toUpperCase())
+}
+
+function addSuffix(data: Data, suffix: string): Data {
+    return data.map(product => product + suffix)
+}
+
+async function fetchAllData(): Promise<any> {
+    const incomingData = await fetchData()
+    const processedData = processData(incomingData)
+    const capitalizedData = capitalizeData(processedData)
+    const suffixedData = addSuffix(capitalizedData, 'FRUIT')
+
+    logData(suffixedData)
+}
+
+fetchAllData()
